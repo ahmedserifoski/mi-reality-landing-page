4373f178784ff18d07c818a889c0075280cc40ab
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2020-06-02 14:03:41 +0200

    head-set width

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -12,14 +12,14 @@ class Home extends React.Component {
 
     render() {
         return (
-            <MDBContainer className="home w-100 d-flex flex-row my-5">
+            <MDBContainer className="home w-100 d-flex flex-column flex-md-row my-5">
                 <MDBContainer className="home2 d-flex flex-column justify-content-left">
                     <h1 className="mi-reality">Mi reality is now going virtual</h1>
                     <p className="p-home grey-text">Best in class virtual reaity gear to make your everyday experience great. Go check it out!</p>
                     <Button text="Buy Now" className="but-home"/>
                 </MDBContainer>
                 
-                <img className="head-set" src={product} alt="Virtual Head Set"/>
+                <img className="head-set d-flex align-self-center" src={product} alt="Virtual Head Set"/>
             </MDBContainer>
         )
     }
